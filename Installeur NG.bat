ee3035e51f564999bb0aebff33de961bab2f5451
Kiyoshi212 <e.kiyoshi212@gmail.com>
2026-06-01 01:20:47 +0200

    first commit

---
+++ Installeur NG.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+:: Dossier principal
+set "NG_DIR=%USERPROFILE%\NationsGlory"
+mkdir "%NG_DIR%" 2>nul
+
+cd /d "%NG_DIR%"
+
+:: Téléchargement
+powershell -Command "Invoke-WebRequest -Uri 'https://nationsglory.fr/download?windows' -OutFile 'NationsGloryInstaller.exe'"
+
+:: Lancement sans demande d'élévation
+set __COMPAT_LAYER=RunAsInvoker
+start "" "NationsGloryInstaller.exe"
+
+echo Attente de la creation du dossier NG...
+
+:WAIT_NG
+if exist "%NG_DIR%\NG" goto NG_FOUND
+timeout /t 5 >nul
+goto WAIT_NG
+
+:NG_FOUND
+echo Dossier NG detecte.
+
+echo Attente de NationsGlory.exe...
+
+:WAIT_EXE
+if exist "%NG_DIR%\NG\NationsGlory.exe" goto EXE_FOUND
+timeout /t 5 >nul
+goto WAIT_EXE
+
+:EXE_FOUND
+echo Executable detecte.
+
+:: Creation du raccourci Bureau
+powershell -Command ^
+"$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%USERPROFILE%\Desktop\NationsGlory.lnk');" ^
+"$s.TargetPath='%NG_DIR%\NG\NationsGlory.exe';" ^
+"$s.WorkingDirectory='%NG_DIR%\NG';" ^
+"$s.Save();"
+
+echo Raccourci cree sur le Bureau.
+echo Termine.
+
+pause
